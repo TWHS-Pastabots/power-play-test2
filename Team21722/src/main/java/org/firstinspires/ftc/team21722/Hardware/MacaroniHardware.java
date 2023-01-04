f8b0305a9c4e31fb6fed3466b8123eae252c9d5d
yaselzarka <yaselzarka@gmail.com>
2023-01-04 14:01:05 -0600

    wee

--- Team21722/src/main/java/org/firstinspires/ftc/team21722/Hardware/MacaroniHardware.java
+++ Team21722/src/main/java/org/firstinspires/ftc/team21722/Hardware/MacaroniHardware.java
@@ -9,29 +9,33 @@ import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.robotcore.internal.system.Assert;
 
 
-public class MacaroniHardware
-{
-    public DcMotorEx leftFront= null;
-    public DcMotorEx rightFront= null;
-    public DcMotorEx leftRear= null;
-    public DcMotorEx rightRear= null;
+public class MacaroniHardware {
+    public DcMotorEx leftFront = null;
+    public DcMotorEx rightFront = null;
+    public DcMotorEx leftRear = null;
+    public DcMotorEx rightRear = null;
     public DcMotorEx[] motors;
 
+    //supplementary motor
+    public DcMotorEx liftArm = null;
+    public Servo clawServo = null;
 
-    public void init( HardwareMap hardwaremap )
+
+    public void init(HardwareMap hardwaremap)
     {
         Assert.assertNotNull(hardwaremap);
         initializeDriveMotors(hardwaremap);
+        initializeSupplementMotor(hardwaremap);
     }
-    public void initializeDriveMotors (HardwareMap hardwaremap)
-    {
+
+    public void initializeDriveMotors(HardwareMap hardwaremap) {
         leftFront = hardwaremap.get(DcMotorEx.class, MacaroniIds.LEFT_FRONT_MOTOR);
         rightFront = hardwaremap.get(DcMotorEx.class, MacaroniIds.RIGHT_FRONT_MOTOR);
         leftRear = hardwaremap.get(DcMotorEx.class, MacaroniIds.LEFT_REAR_MOTOR);
         rightRear = hardwaremap.get(DcMotorEx.class, MacaroniIds.RIGHT_REAR_MOTOR);
 
 
-        motors = new DcMotorEx[] {leftFront, rightRear, leftRear, rightFront};
+        motors = new DcMotorEx[]{leftFront, rightRear, leftRear, rightFront};
 
 
         leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -39,11 +43,30 @@ public class MacaroniHardware
         rightFront.setDirection(DcMotorSimple.Direction.FORWARD);
         rightRear.setDirection(DcMotorSimple.Direction.FORWARD);
 
-        for(DcMotorEx motor : motors)
-        {
+        for (DcMotorEx motor : motors) {
             motor.setPower(0.0);
             motor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
             motor.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
         }
-    } //...
+
+
+    }
+
+    public void initializeSupplementMotor(HardwareMap hardwaremap)
+    {
+        liftArm = hardwaremap.get(DcMotorEx.class, MacaroniIds.LIFT_ARM_MOTOR);
+
+        liftArm.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        liftArm.setPower(0.0);
+        liftArm.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        liftArm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        liftArm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        clawServo = hardwaremap.get(Servo.class, MacaroniIds.CLAW_SERVO);
+
+
+
+
+    }
 }
--- Team21722/src/main/java/org/firstinspires/ftc/team21722/Hardware/MacaroniIds.java
+++ Team21722/src/main/java/org/firstinspires/ftc/team21722/Hardware/MacaroniIds.java
@@ -6,4 +6,11 @@ public class MacaroniIds
     public static final String RIGHT_FRONT_MOTOR  = "rightFront";
     public static final String LEFT_REAR_MOTOR  = "leftRear";
     public static final String RIGHT_REAR_MOTOR  = "rightRear";
+
+    //suplement motors
+    public static final String LIFT_ARM_MOTOR = "liftArm";
+
+
+    //servo motor
+    public static final String CLAW_SERVO =  "clawServo";
 }
